62c7e85bd941686299fb14a82e76701afe9724dc
Damon Salvatore <165358433+MadhavSingh1509@users.noreply.github.com>
2026-07-13 22:10:42 +0530

    Add BST validation function in Validate_bst.cpp
    Implement a function to validate if a binary tree is a BST.

---
+++ Validate_bst.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+void f(TreeNode* root,vector<int>&ans){
+    if(!root)return ;
+    f(root->left,ans);
+    ans.push_back(root->val);
+    f(root->right,ans);
+}
+    bool isValidBST(TreeNode* root) {
+        vector<int>ans;
+        f(root,ans);
+        for(int i=0;i<ans.size()-1;i++){if(ans[i]>=ans[i+1])return false;}
+        return true;
+    }
+};
