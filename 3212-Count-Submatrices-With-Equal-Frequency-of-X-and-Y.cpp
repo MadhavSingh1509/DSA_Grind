42024e1cd7cd80f676a8b803d16e492d54b4f4bc
Damon Salvatore <165358433+MadhavSingh1509@users.noreply.github.com>
2026-08-14 10:27:25 +0530

    [3212] [Time Beats: 53.07%] [Memory Beats: 77.39%] - LeetPush

--- 3212-Count-Submatrices-With-Equal-Frequency-of-X-and-Y.cpp
+++ 3212-Count-Submatrices-With-Equal-Frequency-of-X-and-Y.cpp
@@ -4,42 +4,40 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> pref(m, vector<int>(n));
-        vector<vector<int>> xcnt(m, vector<int>(n));
+        vector<vector<int>> pref(m, vector<int>(n, 0));
+        vector<bool> hasX(n, false);
 
         int ans = 0;
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
 
-                if(grid[i][j] == 'X') {
+            
+                if(grid[i][j] == 'X')
                     pref[i][j] = 1;
-                    xcnt[i][j] = 1;
-                }
-                else if(grid[i][j] == 'Y') {
+                else if(grid[i][j] == 'Y')
                     pref[i][j] = -1;
-                }
 
-                if(i > 0) {
+                
+                if(i > 0)
                     pref[i][j] += pref[i-1][j];
-                    xcnt[i][j] += xcnt[i-1][j];
-                }
 
-                if(j > 0) {
+                if(j > 0)
                     pref[i][j] += pref[i][j-1];
-                    xcnt[i][j] += xcnt[i][j-1];
-                }
 
-                if(i > 0 && j > 0) {
+                if(i > 0 && j > 0)
                     pref[i][j] -= pref[i-1][j-1];
-                    xcnt[i][j] -= xcnt[i-1][j-1];
-                }
 
-                if(pref[i][j] == 0 && xcnt[i][j] > 0)
+                
+                hasX[j] = hasX[j] || (j > 0 && hasX[j-1]) 
+                                   || (grid[i][j] == 'X');
+
+                
+                if(pref[i][j] == 0 && hasX[j])
                     ans++;
             }
         }
-        
+
         return ans;
     }
 };
