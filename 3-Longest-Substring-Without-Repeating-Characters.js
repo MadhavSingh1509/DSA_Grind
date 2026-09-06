2137e8460339eb5e89d398a533e141ccaff92167
Damon Salvatore <165358433+MadhavSingh1509@users.noreply.github.com>
2026-09-06 15:05:47 +0530

    [3] [Time Beats: 26.33%] [Memory Beats: 53.20%] - LeetPush

--- 3-Longest-Substring-Without-Repeating-Characters.js
+++ 3-Longest-Substring-Without-Repeating-Characters.js
@@ -3,13 +3,18 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let st=new Set();
-    let i=0;
-    let ans=0;
-    for(let j=0;j<s.length;j++){
-        while(st.has(s[j])){st.delete(s[i]);i++;}
-        st.add(s[j]);
-        ans=Math.max(ans,j-i+1);}
-        return ans;
-    
+  let i=0,j=0;
+  let n=s.length;
+  let ans=0;
+  let st=new Set();
+  while(j<n){
+    while(i<n&&st.has(s[j])){
+        st.delete(s[i]);
+        i++;
+    }
+    st.add(s[j]);
+    ans=Math.max(ans,j-i+1);
+    j++;
+  }  
+  return ans;
 };
