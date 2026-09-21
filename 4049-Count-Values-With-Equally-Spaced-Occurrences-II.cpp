d1aacdb6cb1ab10abe50467a5f8fdca5fdb4cd82
Damon Salvatore <165358433+MadhavSingh1509@users.noreply.github.com>
2026-09-21 17:14:54 +0530

    [4049] [Time Beats: 5.04%] [Memory Beats: 15.37%] - LeetPush

---
+++ 4049-Count-Values-With-Equally-Spaced-Occurrences-II.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countSpecialIntegers(vector<int>& nums) {
+        map<int,int>m;
+        map<int,int>f;
+        map<int,int>last;
+        unordered_set<int>st;
+        for(auto ele:nums)st.insert(ele);
+        for(int i=0;i<nums.size();i++){
+            if(!st.count(nums[i])){  continue;}
+            if(f[nums[i]]==0){
+                f[nums[i]]++;
+              m[nums[i]]=i;
+last[nums[i]]=i;
+            }
+            else  if(f[nums[i]]==1){
+                f[nums[i]]=2;
+                int diff=i-last[nums[i]];
+                m[nums[i]]=diff;
+                last[nums[i]]=i;
+            }
+            else {
+                f[nums[i]]++;
+                int diff=m[nums[i]];
+                if(diff+last[nums[i]]==i){ last[nums[i]]=i;}
+                else st.erase(nums[i]);
+            }
+        }
+
+       int ans=0;
+       for(auto ele:st)if(f[ele]>=3)ans++;
+       return ans;
+    }
+};
